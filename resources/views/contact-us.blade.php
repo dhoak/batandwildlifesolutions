3747274a8dfff5d365e0a6aa53d7e9d8c3760eef
David Hoak <davidhoak@Davids-MacBook-Pro.local>
2016-02-24 12:50:16 -0500

    added contact page, logo

---
+++ resources/views/contact-us.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+        <title>Contact - Reach Your Bat and Wildlife Professional Today | Bat and Wildlife Solutions</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					@include('header')
+
+				<!-- Main -->
+					<article id="main">
+						<header>
+							<h2>Contact Us</h2>
+							<p>Operators are standing by.</p>
+						</header>
+						<section class="wrapper style5">
+							<div class="inner">
+								<h4>There are muliple ways to reach us.</h4>
+								
+								<p>As a company we pride ourselves on helping you with your bat exclusion or attic restoration needs and will travel state-wide by appointment. Don't hesitate to give us a call, even if your area is not represented below.</p>
+
+								<h3>Phone</h3>
+								<table>
+									<tr>
+										<td style="width: 60%;">Flint & Tri Cities</td>
+										<td>810.354.8880</td>
+									</tr>
+									<tr>
+										<td>Southeast Michigan</td>
+										<td>248.579.9915</td>
+									</tr>
+									<tr>
+										<td>Greater Lansing</td>
+										<td>517.798.6676</td>
+									</tr>
+									<tr>
+										<td>Central & Northeast Michigan</td>
+										<td>989.245.9489</td>
+									</tr>
+								</table>
+
+								<h3>Messaging</h3>
+								<table>
+									<tr>
+										<td style="width: 60%;">Email</td>
+										<td><a hef="mailto:info@batandwildlifesolutions.com">info@batandwildlifesolutions.com</a></td>
+									</tr>
+									<tr>
+										<td>Fax</td>
+										<td>810.354.8881</td>
+									</tr>
+								</table>
+							</div>
+						</section>
+					</article>
+
+				<!-- Footer -->
+					@include('footer')
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
